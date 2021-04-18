31fea87eb2c083e3c856d149848cdf41ed00487d
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2021-04-18 14:26:13 +0300

    Add files via upload

---
+++ Propertiesbox/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Propertiesbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.eBSSecurtyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.kontrolleriAtaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Location = new System.Drawing.Point(597, 12);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.SelectedObject = this;
+            this.propertyGrid1.Size = new System.Drawing.Size(285, 433);
+            this.propertyGrid1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Mesaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(114, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 278);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(579, 176);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(370, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eBSSecurtyToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.kontrolleriAtaToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 76);
+            // 
+            // eBSSecurtyToolStripMenuItem
+            // 
+            this.eBSSecurtyToolStripMenuItem.Enabled = false;
+            this.eBSSecurtyToolStripMenuItem.Name = "eBSSecurtyToolStripMenuItem";
+            this.eBSSecurtyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.eBSSecurtyToolStripMenuItem.Text = "EBS Securty";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // kontrolleriAtaToolStripMenuItem
+            // 
+            this.kontrolleriAtaToolStripMenuItem.Name = "kontrolleriAtaToolStripMenuItem";
+            this.kontrolleriAtaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kontrolleriAtaToolStripMenuItem.Text = "Özellik Değiştirme";
+            this.kontrolleriAtaToolStripMenuItem.Click += new System.EventHandler(this.kontrolleriAtaToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 466);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.propertyGrid1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem eBSSecurtyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem kontrolleriAtaToolStripMenuItem;
+    }
+}
+
---
+++ Propertiesbox/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Propertiesbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var item in this.Controls)
+            {
+                comboBox1.Items.Add(((Control)item).Name);
+            }
+            comboBox1.Hide();
+            propertyGrid1.Hide();
+            propertyGrid1.SelectedObject = dataGridView1.Rows;
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].Name = "İd";
+            dataGridView1.Columns[1].Name = "Adı";
+            dataGridView1.Columns[2].Name = "Numarası";
+            dataGridView1.Rows.Add("1", "Ebubekir Bastama", "055547855475555452545");
+            dataGridView1.Rows.Add("1", "Fentinar esdidar", "0555478554755551455452545");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Selamlar Nasılsınız...","EBS");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Silmek istediğinize eminmisiniz", "EBS", MessageBoxButtons.OKCancel)==DialogResult.OK)
+            {
+                MessageBox.Show("İlgili Mesaj Silindi","EBS");
+            }
+        }
+        bool dgr = true;
+        private void kontrolleriAtaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dgr == true)
+            {
+                propertyGrid1.Show();
+                comboBox1.Show();
+                dgr = false;
+            }
+            else
+            {
+                propertyGrid1.Hide();
+                comboBox1.Hide();
+                dgr = true;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (var item in this.Controls)
+            {
+                if (comboBox1.Text == ((Control)item).Name)
+                {
+                    propertyGrid1.SelectedObject = item;
+                }
+            }
+        }
+    }
+}
